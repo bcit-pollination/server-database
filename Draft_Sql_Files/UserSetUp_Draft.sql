3c129a2caf35d4edf4ac9049f5adc1a7b24af2b0
Amber Ma <agmlammy@gmail.com>
2021-01-28 15:25:37 -0800

    drafted potential users

--- Draft_Sql_Files/UserSetUp_Draft.sql
+++ Draft_Sql_Files/UserSetUp_Draft.sql
@@ -9,3 +9,4 @@ CREATE USER 'ios_and_admin'@'localhost' IDENTIFIED BY 'm0bi13!';
 CREATE USER 'export'@'localhost' IDENTIFIED BY 'inp+0u+P+';
 CREATE USER 'touchscreen'@'localhost' IDENTIFIED BY 'w!Nd0wW03';
 CREATE USER 'display'@'localhost' IDENTIFIED BY 'sh0WM24t';
+
