87e4a49e5bbc78f7986c4eb989deb47c6d6858c9
Amber Ma <agmlammy@gmail.com>
2021-01-31 18:26:56 -0800

    Changed the seupdatabase_with_def file to have status instead of is published, with a default of 0. I also drafted the sql file for creating our users, it is unfinished (so don't try to run it). Will have to communicate with the different teams to determine what permissions to grant

--- Draft_Sql_Files/UserSetUp_Draft.sql
+++ Draft_Sql_Files/UserSetUp_Draft.sql
@@ -4,9 +4,28 @@ DROP USER IF EXISTS 'ios_and_admin'@'localhost';
 DROP USER IF EXISTS 'export'@'localhost';
 DROP USER IF EXISTS 'touchscreen'@'localhost';
 DROP USER IF EXISTS 'display'@'localhost';
+DROP USER IF EXISTS 'adminMa'@'localhost';
+DROP USER IF EXISTS 'adminHuang'@'localhost';
 
 CREATE USER 'ios_and_admin'@'localhost' IDENTIFIED BY 'm0bi13!';
 CREATE USER 'export'@'localhost' IDENTIFIED BY 'inp+0u+P+';
 CREATE USER 'touchscreen'@'localhost' IDENTIFIED BY 'w!Nd0wW03';
 CREATE USER 'display'@'localhost' IDENTIFIED BY 'sh0WM24t';
+CREATE USER 'adminMa'@'localhost' IDENTIFIED BY 'atm0nchu1';
+CREATE USER 'adminHuang'@'localhost' IDENTIFIED BY 'm1ll!n02';
 
+GRANT SELECT ON test_schema.users TO 'ios_and_admin'@'localhost';
+
+GRANT SELECT ON test_schema.* TO 'adminMa'@'localhost';
+GRANT UPDATE ON test_schema.* TO 'adminMa'@'localhost';
+GRANT DELETE ON test_schema.* TO 'adminMa'@'localhost';
+GRANT GRANT OPTION ON test_schema.* TO 'adminMa'@'localhost';
+GRANT INSERT ON test_schema.* TO 'adminMa'@'localhost';
+
+GRANT SELECT ON test_schema.* TO 'adminHuang'@'localhost';
+GRANT UPDATE ON test_schema.* TO 'adminHuang'@'localhost';
+GRANT DELETE ON test_schema.* TO 'adminHuang'@'localhost';
+GRANT GRANT OPTION ON test_schema.* TO 'adminHuang'@'localhost';
+GRANT INSERT ON test_schema.* TO 'adminHuang'@'localhost';
+
+FLUSH PRIVILEGES;
