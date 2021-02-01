af240a3183bd4be906cc71b1dfc69df32b756b73
Amber Ma <agmlammy@gmail.com>
2021-02-01 11:48:40 -0800

    user sql file has been finalized for now

--- Draft_Sql_Files/UserSetUp_Draft.sql
+++ Draft_Sql_Files/UserSetUp_Draft.sql
@@ -1,20 +1,12 @@
 USE test_schema;
 
-DROP USER IF EXISTS 'ios_and_admin'@'localhost';
-DROP USER IF EXISTS 'export'@'localhost';
-DROP USER IF EXISTS 'touchscreen'@'localhost';
-DROP USER IF EXISTS 'display'@'localhost';
+DROP USER IF EXISTS 'server'@'localhost';
 DROP USER IF EXISTS 'adminMa'@'localhost';
 DROP USER IF EXISTS 'adminHuang'@'localhost';
 
-CREATE USER 'ios_and_admin'@'localhost' IDENTIFIED BY 'm0bi13!';
-CREATE USER 'export'@'localhost' IDENTIFIED BY 'inp+0u+P+';
-CREATE USER 'touchscreen'@'localhost' IDENTIFIED BY 'w!Nd0wW03';
-CREATE USER 'display'@'localhost' IDENTIFIED BY 'sh0WM24t';
+CREATE USER 'server'@'localhost' IDENTIFIED BY 'm0bi13!';
 CREATE USER 'adminMa'@'localhost' IDENTIFIED BY 'atm0nchu1';
 CREATE USER 'adminHuang'@'localhost' IDENTIFIED BY 'm1ll!n02';
-
-GRANT SELECT ON test_schema.users TO 'ios_and_admin'@'localhost';
 
 GRANT SELECT ON test_schema.* TO 'adminMa'@'localhost';
 GRANT UPDATE ON test_schema.* TO 'adminMa'@'localhost';
