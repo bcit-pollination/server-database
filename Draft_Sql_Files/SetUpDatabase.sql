3b0f18c2eb8159acae62d65cb5c76e22f52fcfae
Amber Ma <agmlammy@gmail.com>
2021-02-03 12:24:07 -0800

    added DOB and email to database

--- Draft_Sql_Files/SetUpDatabase.sql
+++ Draft_Sql_Files/SetUpDatabase.sql
@@ -15,6 +15,8 @@ CREATE TABLE Users (
     user_id 			INT 			NOT NULL 	AUTO_INCREMENT,
     first_name 			VARCHAR(40) 	NOT NULL,
     last_name 			VARCHAR(40) 	NOT NULL,
+	email               VARCHAR(40)     NOT NULL,
+	DOB                 DATE            NOT NULL,
     voting_token		VARCHAR(36) 	NOT NULL,
     PRIMARY KEY (user_id)
 );
