77445ab694882e634b8ed65f1b0aeb9148b23fc0
Amber Ma <agmlammy@gmail.com>
2021-02-03 13:01:00 -0800

    added the first procedure

--- Draft_Sql_Files/Select_Procedures.sql
+++ Draft_Sql_Files/Select_Procedures.sql
@@ -8,6 +8,25 @@ DELIMITER //
 CREATE get_user(IN id INT)
 BEGIN
 	SELECT first_name, last_name, voting_token FROM users
-	
+	WHERE user_id = id;
+END //
+DELIMITER ;
+
+/** Gets the data from the organizations that the user
+	belongs to from the user's id. */
+DELIMITER //
+CREATE get_user_organization(IN id INT)
+BEGIN
+	SELECT first_name, last_name, voting_token FROM users
+	WHERE user_id = id;
+END //
+DELIMITER ;
+
+/** Gets the elections that are available to user.*/
+DELIMITER //
+CREATE get_user_organization(IN id INT)
+BEGIN
+	SELECT first_name, last_name, voting_token FROM users
+	WHERE user_id = id;
 END //
 DELIMITER ;
