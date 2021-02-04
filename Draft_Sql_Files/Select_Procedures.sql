b00b94337a74fb9a5adefb302a3438bf5ea5322c
Amber Ma <agmlammy@gmail.com>
2021-02-03 18:48:03 -0800

    these are my finished drafts for the procedures

--- Draft_Sql_Files/Select_Procedures.sql
+++ Draft_Sql_Files/Select_Procedures.sql
@@ -2,41 +2,80 @@ DROP PROCEDURE IF EXISTS get_user;
 DROP PROCEDURE IF EXISTS get_user_organization;
 DROP PROCEDURE IF EXISTS get_user_elections;
 DROP PROCEDURE IF EXISTS get_organization_users;
+DROP PROCEDURE IF EXISTS get_user_elections_alternate;
 
-/** Gets user data from the user's id. */
+/** Takes in a user id, and returns the user's data
+	(non-sensitive data). */
 DELIMITER //
-CREATE get_user(IN id INT)
+CREATE PROCEDURE get_user(IN id INT)
 BEGIN
-	SELECT first_name, last_name, voting_token FROM users
+	SELECT user_id, first_name, last_name, email, DOB, voting_token FROM users
 	WHERE user_id = id;
 END //
 DELIMITER ;
 
-/** Gets the data from the organizations that the user
-	belongs to from the user's id. */
+/** Takes in a user's id, and returns the data of the organizations
+	that the user specified belongs to.*/
 DELIMITER //
-CREATE get_user_organization(IN id INT)
+CREATE PROCEDURE get_user_organization(IN id INT)
 BEGIN
-	SELECT first_name, last_name, voting_token FROM users
-	WHERE user_id = id;
+	SELECT o.org_id, o.org_name, e.privilege_level FROM users u
+		INNER JOIN enrollment e
+			ON u.user_id = e.user_id
+		INNER JOIN organization o
+			ON e.org_id = o.org_id
+	WHERE e.user_id = id;
 END //
 DELIMITER ;
 
-/** Gets the elections that are available to user.*/
+/** Takes in a user's id, and gets the elections 
+    that are available to them.*/
 DELIMITER //
-CREATE get_user_organization(IN id INT)
+CREATE PROCEDURE get_user_elections(IN id INT)
 BEGIN
-	SELECT first_name, last_name, voting_token FROM users
-	WHERE user_id = id;
+	SELECT election_id, el.org_id, start_time,
+		end_time, status, is_anonymous FROM users u
+			INNER JOIN enrollment e
+				ON e.user_id = u.user_id
+			INNER JOIN election el
+				ON e.org_id = el.org_id	
+	WHERE e.user_id = id;
 END //
 DELIMITER ;
 
-/** Gets the elections that are available to user.*/
+
+/** Takes in the id of an organization as a parameter, and
+	displays all of the users who are currently in that organization.
+	This includes their id, firstname, lastname, email, DOB, and voting_token
+	(I avoided any sensitive information such as their passwords of course).*/
 DELIMITER //
-CREATE get_user_organization(IN id INT)
+CREATE PROCEDURE get_organization_users(IN id INT)
 BEGIN
-	SELECT first_name, last_name, voting_token FROM users
-	WHERE user_id = id;
+	SELECT e.user_id, first_name, last_name, email, DOB, voting_token FROM users u
+		INNER JOIN enrollment e
+		   ON u.user_id = e.user_id
+		INNER JOIN organization o
+		   ON o.org_id = e.org_id
+	WHERE o.org_id = id;
 END //
 DELIMITER ;
 
+/** A combined version of the first three functions,
+	which was what was asked for on the queries document.
+	In this, all the elections that the user is associated with
+	are listed alongside the organization they belong to, as well
+	as the election.*/
+		DELIMITER //
+	CREATE PROCEDURE get_user_elections_alternate(IN id INT)
+	BEGIN
+		SELECT e.user_id, e.org_id, election_id, privilege_level, 
+		start_time, end_time, status, is_anonymous FROM users u
+		INNER JOIN enrollment e
+			ON e.user_id = u.user_id
+		INNER JOIN election el
+			ON el.org_id = e.org_id
+		WHERE e.user_id = id;
+		
+	END //
+	DELIMITER ;	
+	
