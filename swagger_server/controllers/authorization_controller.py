de04908c16085758e540d062a804c879b88aec61
KarelChanivecky <Karl_Chanivecky@hotmail.com>
2021-02-07 08:11:31 -0800

    Added some incomplete instructions on how to install server

--- swagger_server/controllers/authorization_controller.py
+++ swagger_server/controllers/authorization_controller.py
@@ -1,9 +1,15 @@
-from typing import List
+from werkzeug.exceptions import Unauthorized
+
+from src.auth.jwt import decode_token
+
 """
 controller generated to handled auth operation described at:
 https://connexion.readthedocs.io/en/latest/security.html
 """
+
+
 def check_jwt(token):
-    return {'test_key': 'test_value'}
-
-
+    try:
+        return decode_token(token)
+    except Unauthorized as not_auth:
+        return None
