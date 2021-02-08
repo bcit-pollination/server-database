24b5c619eca3dd334acbf6583a526ace2d6b39b5
karelchanivecky <karl_chanivecky@hotmail.com>
2021-02-07 23:48:53 -0800

    Created sample function to query db

--- src/db/mysql_interface.py
+++ src/db/mysql_interface.py
@@ -16,8 +16,24 @@ def get_uid_with_credentials(email, password):
 def create_user(user: User):
     with get_db_connection() as db:
         c = db.cursor()
-        c.execute("")
-        return c.fetchone()
+        c.execute("""CALL CreateUser(
+        %(first_name),
+        %(last_name),
+        %(email),
+        %(DOB),
+        %(password_salt),
+        %(password_hash),
+        %(voting_token))""",
+                  (user.first_name,
+                   user.last_name,
+                   user.email,
+                   user.dob,
+                   user.password,
+                   "foo",
+                   "vote with this!"))
+        result = c.fetchone()
+        print(result)
+        return result
 
 
 def get_user(uid):
