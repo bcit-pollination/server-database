00f097e696d961159dbb69b3a3e5c39d5244d01d
Amber Ma <agmlammy@gmail.com>
2021-02-08 19:49:34 -0800

    Re-organized procedures

--- Draft_Sql_Files/CreateStoredProcedures.sql
+++ Draft_Sql_Files/CreateStoredProcedures.sql
@@ -1,26 +1,45 @@
-DROP PROCEDURE IF EXISTS GetUser;
-DROP PROCEDURE IF EXISTS GetUserOrganization;
-DROP PROCEDURE IF EXISTS GetUserElections;
-DROP PROCEDURE IF EXISTS GetOrganizationUsers;
-DROP PROCEDURE IF EXISTS GetUserElectionsAlternate;
-DROP PROCEDURE IF EXISTS EnrollUser;
-DROP PROCEDURE IF EXISTS CreateOrg;
-DROP PROCEDURE IF EXISTS CreateUser;
 DROP PROCEDURE IF EXISTS LoginUser;
+DROP PROCEDURE IF EXISTS GetUser;
+DROP PROCEDURE IF EXISTS GetUserToken;
+DROP PROCEDURE IF EXISTS CreateUser;
+
+DROP PROCEDURE IF EXISTS GetOrganizations;
+DROP PROCEDURE IF EXISTS GetOrganization;
+DROP PROCEDURE IF EXISTS CreateOrg;
+
+DROP PROCEDURE IF EXISTS GetInvitedUsers;
+DROP PROCEDURE IF EXISTS GetJoinedUsers;
+DROP PROCEDURE IF EXISTS GetAllUsers;
+DROP PROCEDURE IF EXISTS InviteUser;
+DROP PROCEDURE IF EXISTS AcceptInvite;
+DROP PROCEDURE IF EXISTS KickUser;
+DROP PROCEDURE IF EXISTS UpdatePrivilege;
+
+DROP PROCEDURE IF EXISTS GetRPIList;
+DROP PROCEDURE IF EXISTS CreateRPI;
+DROP PROCEDURE IF EXISTS AssignRPI;
+DROP PROCEDURE IF EXISTS DeleteRPI;
+
+DROP PROCEDURE IF EXISTS GetLocationList;
+DROP PROCEDURE IF EXISTS CreateLocation;
+DROP PROCEDURE IF EXISTS UpdateLocation;
+DROP PROCEDURE IF EXISTS DeleteLocation;
+
+DROP PROCEDURE IF EXISTS GetElectionListOrg;
+DROP PROCEDURE IF EXISTS GetElectionListUser;
+DROP PROCEDURE IF EXISTS GetElection;
 DROP PROCEDURE IF EXISTS CreateElection;
+DROP PROCEDURE IF EXISTS UpdateElection;
+DROP PROCEDURE IF EXISTS DeleteElection;
+
+
 
 DELIMITER //
-CREATE PROCEDURE CreateElection(
-	IN org_id INT, 
-    IN description VARCHAR(40),
-    IN start_time TIMESTAMP,
-    IN end_time TIMESTAMP,
-    IN is_anonymous BOOLEAN)
-BEGIN
-	INSERT INTO Election(org_id, description, start_time, end_time, is_anonymous)
-    VALUES(org_id, description, start_time, end_time, is_anonymous);
-	SELECT LAST_INSERT_ID();
-END;//
+
+/** 
+	Takes in a user's login credentials,
+	then returns the user's id
+	*/
 
 CREATE PROCEDURE LoginUser(
 	IN in_email VARCHAR(40),
@@ -32,6 +51,35 @@ BEGIN
 END;//
 
 
+
+
+
+/** Takes in a user id, and returns the user's data
+	(non-sensitive data). */
+CREATE PROCEDURE GetUser(IN id INT)
+BEGIN
+	SELECT user_id, first_name, last_name, email, date_of_birth FROM users
+	WHERE user_id = id;
+END; //
+
+
+
+
+
+/** Takes in a user id, and returns the user's token
+	(non-sensitive data). */
+CREATE PROCEDURE GetUser(IN id INT)
+BEGIN
+	SELECT voting_token FROM users
+	WHERE user_id = id;
+END; //
+
+
+
+
+
+/** Takes in user information, and uses it
+	to create a user. */
 CREATE PROCEDURE CreateUser(
 	IN first_name VARCHAR(40), 
     IN last_name VARCHAR(40), 
@@ -47,6 +95,40 @@ BEGIN
 	SELECT LAST_INSERT_ID();
 END; //
 
+	
+
+
+
+/** Takes in a user's id, and returns the data of the organizations
+	that the user specified belongs to.*/
+CREATE PROCEDURE GetOrganizations(IN id INT)
+BEGIN
+	SELECT e.privilege_level, o.org_id, o.org_name FROM users u
+		INNER JOIN enrollment e
+			ON u.user_id = e.user_id
+		INNER JOIN organization o
+			ON e.org_id = o.org_id
+	WHERE e.user_id = id;
+END; //
+
+
+
+
+
+/** Gets an organization's data from
+	an organization id.*/
+CREATE PROCEDURE GetOrganization(IN id INT)
+BEGIN
+	SELECT * FROM organization
+	WHERE org_id = id;
+END; //
+
+
+
+
+
+/** Takes in a user id and a name for the organization,
+	then uses this information to create an organization.*/
 CREATE PROCEDURE CreateOrg(
 	IN user_id INT, 
     IN org_name VARCHAR(40))
@@ -58,6 +140,43 @@ BEGIN
 	VALUES(user_id, LAST_INSERT_ID());
 END; //
 
+
+
+
+/** Takes in the id of an organization as a parameter, and
+	displays all of the users who are currently in that organization.
+	This includes their id, firstname, lastname, email, date_of_birth, and voting_token
+	(I avoided any sensitive information such as their passwords of course).*/
+
+CREATE PROCEDURE GetInvitedUsers(IN id INT)
+BEGIN
+	SELECT e.user_id, first_name, last_name, email, date_of_birth, voting_token, e.privilege FROM users
+		INNER JOIN enrollment e
+		   ON u.user_id = e.user_id
+		INNER JOIN organization o
+		   ON o.org_id = e.org_id
+	WHERE o.org_id = id
+	AND e.privilege_level = 0;
+END; //
+
+
+
+
+/** Takes in information, and uses it to create an election.*/
+CREATE PROCEDURE CreateElection(
+	IN org_id INT, 
+    IN description VARCHAR(40),
+    IN start_time TIMESTAMP,
+    IN end_time TIMESTAMP,
+    IN is_anonymous BOOLEAN)
+BEGIN
+	INSERT INTO Election(org_id, description, start_time, end_time, is_anonymous)
+    VALUES(org_id, description, start_time, end_time, is_anonymous);
+	SELECT LAST_INSERT_ID();
+END;//
+
+/** Takes in the id of a user and an organization, then 
+	enrolls a user into an organization.*/
 CREATE PROCEDURE EnrollUser(
 	IN user_id INT, 
     IN org_id INT)
@@ -67,33 +186,10 @@ BEGIN
 	SELECT LAST_INSERT_ID();
 END; //
 
-/** Takes in a user id, and returns the user's data
-	(non-sensitive data). */
-DELIMITER //
-CREATE PROCEDURE GetUser(IN id INT)
-BEGIN
-	SELECT user_id, first_name, last_name, email, date_of_birth, voting_token FROM users
-	WHERE user_id = id;
-END //
-DELIMITER ;
-
-/** Takes in a user's id, and returns the data of the organizations
-	that the user specified belongs to.*/
-DELIMITER //
-CREATE PROCEDURE GetUserOrganization(IN id INT)
-BEGIN
-	SELECT o.org_id, o.org_name, e.privilege_level FROM users u
-		INNER JOIN enrollment e
-			ON u.user_id = e.user_id
-		INNER JOIN organization o
-			ON e.org_id = o.org_id
-	WHERE e.user_id = id;
-END //
-DELIMITER ;
 
 /** Takes in a user's id, and gets the elections 
     that are available to them.*/
-DELIMITER //
+
 CREATE PROCEDURE GetUserElections(IN id INT)
 BEGIN
 	SELECT election_id, el.org_id, start_time,
@@ -103,32 +199,13 @@ BEGIN
 			INNER JOIN election el
 				ON e.org_id = el.org_id	
 	WHERE e.user_id = id;
-END //
-DELIMITER ;
-
-
-/** Takes in the id of an organization as a parameter, and
-	displays all of the users who are currently in that organization.
-	This includes their id, firstname, lastname, email, date_of_birth, and voting_token
-	(I avoided any sensitive information such as their passwords of course).*/
-DELIMITER //
-CREATE PROCEDURE GetOrganizationUsers(IN id INT)
-BEGIN
-	SELECT e.user_id, first_name, last_name, email, date_of_birth, voting_token FROM users
-		INNER JOIN enrollment e
-		   ON u.user_id = e.user_id
-		INNER JOIN organization o
-		   ON o.org_id = e.org_id
-	WHERE o.org_id = id;
-END //
-DELIMITER ;
+END; //
 
 /** A combined version of the first three functions,
 	which was what was asked for on the queries document.
 	In this, all the elections that the user is associated with
 	are listed alongside the organization they belong to, as well
 	as the election.*/
-		DELIMITER //
 	CREATE PROCEDURE GetUserElectionsAlternate(IN id INT)
 	BEGIN
 		SELECT e.user_id, e.org_id, election_id, privilege_level, 
@@ -139,19 +216,7 @@ DELIMITER ;
 			ON el.org_id = e.org_id
 		WHERE e.user_id = id;
 		
-	END //
-	DELIMITER ;	
-
-GRANT EXECUTE ON PROCEDURE GetUser TO 'server'@'localhost';
-GRANT EXECUTE ON PROCEDURE GetUserOrganization TO 'server'@'localhost';
-GRANT EXECUTE ON PROCEDURE GetUserElections TO 'server'@'localhost';
-GRANT EXECUTE ON PROCEDURE GetOrganizationUsers TO 'server'@'localhost';
-GRANT EXECUTE ON PROCEDURE GetUserElectionsAlternate TO 'server'@'localhost';
-GRANT EXECUTE ON PROCEDURE CreateOrg TO 'server'@'localhost';
-GRANT EXECUTE ON PROCEDURE CreateUser TO 'server'@'localhost';
-GRANT EXECUTE ON PROCEDURE EnrollUser TO 'server'@'localhost';
-GRANT EXECUTE ON PROCEDURE LoginUser TO 'server'@'localhost';
-GRANT EXECUTE ON PROCEDURE CreateElection TO 'server'@'localhost';
+	END; //
 
 FLUSH PRIVILEGES;
 
