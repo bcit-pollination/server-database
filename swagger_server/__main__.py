5a512bdc63f087d747fbebb1de21d6b754207f78
KarelChanivecky <Karl_Chanivecky@hotmail.com>
2021-02-10 08:07:31 -0800

    trying out ssl

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -8,8 +8,8 @@ from swagger_server import encoder
 
 def main():
     context = SSL.Context(SSL.SSLv23_METHOD)
-    context.use_privatekey_file('/etc/letsencrypt/live/pollination.live/fullchain.pem')
-    context.use_certificate_file('/etc/letsencrypt/live/pollination.live/privkey.pem')
+    context.use_privatekey_file('/etc/letsencrypt/live/pollination.live/pollinaiton.live.key')
+    context.use_certificate_file('/etc/letsencrypt/live/pollination.live/pollination.live.crt')
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Pollination Web Server'}, pythonic_params=True)
