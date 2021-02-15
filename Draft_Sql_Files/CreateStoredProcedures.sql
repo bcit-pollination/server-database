0791a083822d5cb3bffbbfe7accf67b48ef3b55f
Amber Ma <agmlammy@gmail.com>
2021-02-14 22:00:48 -0800

    I've added onto the procedure testing file with some of my procedures (excluding drop procedures, which are difficult to fully test alongside the code we have:

--- Draft_Sql_Files/CreateStoredProcedures.sql
+++ Draft_Sql_Files/CreateStoredProcedures.sql
@@ -23,18 +23,31 @@ DROP PROCEDURE IF EXISTS CreateLocation;
 DROP PROCEDURE IF EXISTS UpdateLocation;
 DROP PROCEDURE IF EXISTS DeleteLocation;
 
-DROP PROCEDURE IF EXISTS GetElectionListOrg;
+DROP PROCEDURE IF EXISTS GetElectionListOrg; /** Tested */
 DROP PROCEDURE IF EXISTS GetElectionListUser;
 DROP PROCEDURE IF EXISTS GetElection;
-DROP PROCEDURE IF EXISTS CreateElection;
-DROP PROCEDURE IF EXISTS UpdateElection;
-DROP PROCEDURE IF EXISTS DeleteElection;
+DROP PROCEDURE IF EXISTS UpdateElection; /** Tested*/
+DROP PROCEDURE IF EXISTS DeleteElection; /** Tested*/
+DROP PROCEDURE IF EXISTS CreateElection; /** Tested */
 
 DROP PROCEDURE IF EXISTS CreateVote;
 DROP PROCEDURE IF EXISTS CreateSelection;
 DROP PROCEDURE IF EXISTS GetPublicElectionList;
 
 DROP PROCEDURE IF EXISTS GetElectionsAlternate;
+
+DROP PROCEDURE IF EXISTS GetQuestionChoice; /** Tested */
+DROP PROCEDURE IF EXISTS GetElectionQuestions; /** Tested */
+DROP PROCEDURE IF EXISTS GetElection; /** Tested */
+
+DROP PROCEDURE IF EXISTS AddQuestion; /** Tested */
+DROP PROCEDURE IF EXISTS DropQuestion; /** Tested */
+DROP PROCEDURE IF EXISTS UpdateQuestion; /** Tested*/
+
+DROP PROCEDURE IF EXISTS AddChoice; /** Tested */
+DROP PROCEDURE IF EXISTS DropChoice; /** Tested*/
+DROP PROCEDURE IF EXISTS UpdateChoice; /** Tested. */
+DROP PROCEDURE IF EXISTS GetQuestionSelection; /** Tested */
 
 DELIMITER //
 
@@ -320,4 +333,196 @@ END; //
 		WHERE e.user_id = id;
 		
 	END; //
+	
+/** Gets all the potential answers to questions specified in
+	param.*/
+	
+CREATE PROCEDURE GetQuestionChoice(
+	IN id INT
+	)
+BEGIN
+	SELECT * FROM Choice c
+		INNER JOIN Question q
+		ON c.question_id = q.question_id
+	WHERE c.question_id = id;
+END; //
+
+/** Gets all of the questions from a specific election.*/
+
+CREATE PROCEDURE GetElectionQuestions(
+	IN id INT
+	)
+BEGIN
+	SELECT q.question_id, q.election_id, q.description, q.selection_limit, q.is_required FROM Question q
+		INNER JOIN Election el
+		ON el.election_id = q.election_id
+		WHERE q.election_id = id;
+END; //
+
+/** Gets elections from an organization id. */
+
+CREATE PROCEDURE GetElectionListOrg(
+	IN id INT
+	)
+BEGIN
+	SELECT * FROM Election
+		WHERE org_id = id;
+END; //
+
+/** Gets an election from an election id. */
+
+CREATE PROCEDURE GetElection(
+	IN id INT
+	)
+BEGIN
+	SELECT * FROM Election
+		WHERE election_id = id;
+END; //
+
+
+/** Takes in several values as a parameter,
+	then updates their corresponding fields.
+	*/
+
+CREATE PROCEDURE UpdateElection(
+	IN id INT,
+	IN description VARCHAR(40),
+	IN verified BOOLEAN,
+	IN start_time TIMESTAMP,
+	IN end_time TIMESTAMP,
+	IN is_anonymous BOOLEAN,
+	IN is_public BOOLEAN
+	)
+BEGIN
+	UPDATE Election
+		SET description = description
+		WHERE election_id = id;
+		
+	UPDATE Election
+		SET verified = verified
+		WHERE election_id = id;
+	
+	UPDATE Election
+		SET start_time = start_time
+		WHERE election_id = id;
+		
+	UPDATE Election
+		SET end_time = end_time
+		WHERE election_id = id;
+		
+	UPDATE Election
+		SET is_anonymous = is_anonymous
+		WHERE election_id = id;
+		
+	UPDATE Election
+		SET is_public = is_public
+		WHERE election_id = id;
+END; //
+
+/** Takes in an election's id, and deletes the election
+	attached to it.*/
+
+CREATE PROCEDURE DeleteElection(
+	IN id INT
+	)
+BEGIN
+	DELETE FROM Election
+	WHERE election_id = id;
+END; //
+
+/** Adds question to election. */
+
+CREATE PROCEDURE AddQuestion(
+	IN el_id INT,
+	IN descr VARCHAR(40),
+	IN sel INT,
+	IN req TINYINT(1)
+)
+BEGIN
+	INSERT INTO Question (election_id, description, selection_limit, is_required)
+		VALUES (el_id, descr, sel, req);
+END; //
+
+/** Drops Question from an election. */
+
+CREATE PROCEDURE DropQuestion(
+	IN id INT
+)
+BEGIN
+	DELETE FROM Question
+	WHERE question_id = id;
+END; //
+
+/** Updates a question. */
+
+CREATE PROCEDURE UpdateQuestion(
+	IN id INT,
+	IN description VARCHAR(40),
+	IN is_required TINYINT(1)
+)
+BEGIN
+
+	UPDATE Question
+		SET description = description
+		WHERE question_id = id;
+		
+	UPDATE Question
+		SET is_required = is_required
+		WHERE question_id = id;
+	
+END; //
+
+/** Adds a choice to a question. */
+
+CREATE PROCEDURE AddChoice(
+	IN id INT,
+	IN descr VARCHAR(40)
+)
+BEGIN
+	INSERT INTO Choice (question_id, description) 
+	VALUES(id, descr);
+	
+	UPDATE Question
+		SET selection_limit = selection_limit + 1
+		WHERE question_id = id;
+	
+END; //
+
+/** Drops a choice from a question. */
+
+CREATE PROCEDURE DropChoice(
+	IN id INT
+)
+BEGIN
+
+	UPDATE Question q
+	INNER JOIN Choice c ON q.question_id = c.question_id
+	SET selection_limit = selection_limit - 1
+	WHERE c.choice_id = id;
+	
+	DELETE FROM Choice
+	WHERE choice_id = id; 
+	
+END; //
+
+/** Updates choice for question.*/
+
+CREATE PROCEDURE UpdateChoice(
+	IN id INT,
+	IN descr VARCHAR(40)
+)
+BEGIN
+	UPDATE Choice
+	SET description = descr
+	WHERE choice_id = id;
+END; //
+
+CREATE PROCEDURE GetQuestionSelection(
+	IN id INT
+)
+BEGIN
+	SELECT * FROM Choice
+		WHERE question_id = id;
+END; //
+
 	
--- Draft_Sql_Files/ProceduresTestDatabase.sql
+++ Draft_Sql_Files/ProceduresTestDatabase.sql
@@ -19,4 +19,32 @@ CALL InviteUser(2, 2); /* Bruce is invited to COMP1537 Discord */
 CALL UpdatePrivilege(1, 2, 2); /* Albert is promoted to admin */
 CALL UpdatePrivilege(2, 2, 1); /* Bruce is promoted to member */
 
-CALL CreateElection(2, "Web Dev", CURRENT_TIMESTAMP(), TIMESTAMPADD(hour, 1, CURRENT_TIMESTAMP()), TRUE, FALSE);
+CALL CreateElection(2, "Web Dev", CURRENT_TIMESTAMP(), TIMESTAMPADD(hour, 1, CURRENT_TIMESTAMP()), TRUE, FALSE); /** Creates an Election for COMP1537 Discord*/
+CALL CreateElection(1, "C Survey", CURRENT_TIMESTAMP(), TIMESTAMPADD(hour, 1, CURRENT_TIMESTAMP()), TRUE, FALSE); /** Creates an Election for COMP2510 Study Group*/
+
+CALL AddQuestion(
+1,
+'What language are you good at?',
+0,
+0); /** Adds new question to our first election. */
+
+CALL AddChoice(
+1,
+'Javascript'); /** First choice is added. */
+
+CALL AddChoice(
+1,
+'CSS'); /** Second choice is added. */
+
+CALL AddChoice(
+1,
+'HTML'); /** Third choice is added. */
+
+
+CALL UpdateChoice(2, 'PHP'); /** Changes Choice 'CSS' to 'PHP'*/
+
+CALL UpdateQuestion(1, 'What language is the best?', 1); /** Changes the question. */
+
+CALL UpdateElection(1, 'Web Dev Survey', FALSE, '2021-10-05', '2021-11-09', TRUE, FALSE); /** Updates election name to web dev survey. */
+
+
