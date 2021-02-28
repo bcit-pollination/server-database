7d1099ed6318355a5c845e4f1fbf87a08b77d5f7
karelchanivecky <karl_chanivecky@hotmail.com>
2021-02-27 22:53:14 -0800

    Completed authorization controllers, needs testing

--- server/src/auth/authorization_control.py
+++ server/src/auth/authorization_control.py
@@ -1,28 +1,48 @@
 import six
-from werkzeug.exceptions import Unauthorized
+from werkzeug.exceptions import Unauthorized, BadRequest
 
-import jwt
+import server.src.auth.jwt as jwt
 import connexion
 import server.src.db.mysql_interface as db
-
+import server.src.obj_keys as obj_keys
 """
 controller generated to handled auth operation described at:
 https://connexion.readthedocs.io/en/latest/security.html
 """
-def check_admin(token):
-    return {'test_key': 'test_value'}
+
+MEMBER_PRIV = 1
+ADMIN_PRIV = 2
+OWNER_PRIV = 3
+
 
 def check_member(token):
-    return {'test_key': 'test_value'}
+    token = check_user(token)
+    body = connexion.request.get_json()
+    if obj_keys.ORG_ID not in body:
+        raise BadRequest("Must include org_id in the body")
+    if db.get_privilege(token[jwt.JWT_UID_KEY], body[obj_keys.ORG_ID]) < MEMBER_PRIV:
+        raise Unauthorized("You must be an organization member to access this endpoint")
+    return token
+
+
+def check_admin(token):
+    token = check_user(token)
+    body = connexion.request.get_json()
+    if obj_keys.ORG_ID not in body:
+        raise BadRequest("Must include org_id in the body")
+    if db.get_privilege(token[jwt.JWT_UID_KEY], body[obj_keys.ORG_ID]) < ADMIN_PRIV:
+        raise Unauthorized("You must be an organization administrator to access this endpoint")
+    return token
+
 
 def check_owner(token):
-    token = jwt.decode_token(token)
-    if not db.get_user(token[jwt.JWT_UID_KEY]):
-        raise Unauthorized("User is not found in db")
+    token = check_user(token)
     body = connexion.request.get_json()
-
-    privilege =
-    return {'test_key': 'test_value'}
+    if obj_keys.ORG_ID not in body:
+        raise BadRequest("Must include org_id in the body")
+    if db.get_privilege(token[jwt.JWT_UID_KEY], body[obj_keys.ORG_ID]) < OWNER_PRIV:
+        raise Unauthorized("You must be the organization owner to access this endpoint")
+    return token
 
 
 def check_user(token):
--- server/swagger_server/controllers/authorization_controller.py
+++ server/swagger_server/controllers/authorization_controller.py
@@ -1,20 +1,24 @@
 from typing import List
 import connexion
+import server.src.auth.authorization_control as auth_ctl
 
 """
 controller generated to handled auth operation described at:
 https://connexion.readthedocs.io/en/latest/security.html
 """
+
+
 def check_admin(token):
-    return {'test_key': 'test_value'}
+    return auth_ctl.check_admin(token)
+
 
 def check_member(token):
-    return {'test_key': 'test_value'}
+    return auth_ctl.check_member(token)
+
 
 def check_owner(token):
-    return {'test_key': 'test_value'}
+    return auth_ctl.check_owner(token)
+
 
 def check_user(token):
-    return {'test_key': 'test_value'}
-
-
+    return auth_ctl.check_user(token)
