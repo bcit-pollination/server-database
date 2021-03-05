d30882be5da1ed59ded4c9697b0aa15c79bf66b5
BGetz <bgetz@ascendentgroup.com>
2021-03-04 20:43:40 -0700

    Fixed merge mistake I made for available procedures

--- server/src/db/procedures.py
+++ server/src/db/procedures.py
@@ -1,6 +1,8 @@
 class PROCEDURE:
     """
     List of available procedures available in the Pollination MySql database.
+
+    https://docs.google.com/document/d/1ny0d3P38eVzZHJG2Odj71t52vtQhlIupdYiwLjgoMec/edit
     """
     # name                    = proc_name                   # procedure parameters
     LOGINUSER                 = 'LoginUser'                 # email, password
@@ -8,15 +10,15 @@ class PROCEDURE:
     CREATEORG                 = 'CreateOrg'                 # user_id, org_name, user_org_id, verifier_password
     ENROLLUSER                = 'EnrollUser'                # user_id, user_org_id, email
     GETUSER                   = 'GetUser'                   # user_id
-    GETUSERORGANIZATION       = 'GetUserOrganization'       # user_id
+    GETUSERORGANIZATIONS      = 'GetUserOrgListInfo'        # user_id
     GETUSERELECTIONS          = 'GetUserElections'          # user_id
-    GETORGANIZATIONUSERS      = 'GetOrganizationUsers'      # org_id
-    GETUSERELECTIONSALTERNATE = 'GetUserElectionsAlternate' # user_id
+    GETORGANIZATIONUSERS      = 'GetUsersFromOrg'           # org_id
+    GETUSERELECTIONSALTERNATE = 'GetElectionsAlternate'     # user_id
     UPDATEUSER                = 'UpdateUser'                # user_id, first_name, last_name, email, password
     DEACTIVATEUSER            = 'DeactivateUser'            # user_id
     GETUSERTOKEN              = 'GetUserToken'              # user_id
-    GETORGANIZATIONS          = 'GetOrganizations'          # user_id
-    GETORGANIZATION           = 'GetOrganization'           # org_id
+    GETUSERORGLIST            = 'GetUserOrgListInfo'        # user_id
+    GETORGANIZATION           = 'GetOrg'                    # org_id
     UPDATEORGANIZATION        = 'UpdateOrg'                 # org_id, org_name, verifier_password
     DISBANDORG                = 'DisbandOrg'                # org_id
     GETVERIFIERPASSWORD       = 'GetVerifierPassword'       # org_id
@@ -27,21 +29,22 @@ class PROCEDURE:
     UPDATEELECTION            = 'UpdateElection'            # election_id, description, start_time, end_time, status, is_public, is_anonymous, questions
     DELETEELECTION            = 'DeleteElection'            # election_id
     GETELECTION               = 'GetElection'               # election_id
-    GETELECTIONLISTORG        = 'GetElectionListOrg'        # org_id
+    GETELECTIONLISTORG        = 'GetOrgElections'           # org_id
     GETELECTIONLISTUSER       = 'GetElectionListUser'       # user_id
-    GETINDIVIDUALVOTES        = 'GetIndividualVotes'        # election_id
+    GETINDIVIDUALVOTES        = 'GetUserVotes'              # election_id
     GETELECTIONSATLERNATE     = 'GetElectionsAlternate'     # user_id
-    GETQUESTIONOPT            = 'GetQuestionOpt'            # question_id
-    GETELECTIONQUESTIONS      = 'GetQuestions'      # election_id
+    GETQUESTIONOPT            = 'GetQuestionOptions'        # question_id
+    GETELECTIONQUESTIONS      = 'GetQuestions'              # election_id
     GETPUBLICELECTIONS        = 'GetPublicElections'        # NOTHING
     ADDQUESTION               = 'AddQuestion'               # election_id, description
-    DROPQUESTION              = 'DropQuestion'              # question_id
+    DROPQUESTION              = 'DeleteQuestion'            # question_id
     UPDATEQUESTION            = 'UpdateQuestion'            # question_id, description
-    ADDOPT                    = 'AddOpt'                    # opt_id
-    DROPOPT                   = 'DropOpt'                   # opt_id
+    UPDATEQUESTIONOPT         = 'UpdateOption'              # option_id, description
+    ADDOPT                    = 'AddOption'                 # opt_id
+    DELETEOPTION              = 'DeleteOption'              # opt_id
     UPDATEOPT                 = 'UpdateOpt'                 # opt_id, description
     GETPRIVILEGE              = 'GetPrivilege'              # org_id, user_id
     GETIDVT                   = 'GetIdVt'                   # election_id
-    CREATEVOTE                = 'CreateVote'                # voting_token, time_stamp, election_id
-    CREATECHOICE              = 'CreateChoice'              # vote_id, opt_id
+    ADDVOTE                   = 'AddVote'                   # voting_token, time_stamp, election_id
+    ADDCHOICE                 = 'AddChoice'                 # vote_id, opt_id
     GETOWNERORGINFO           = 'GetOwnerOrgInfo'           # uid
